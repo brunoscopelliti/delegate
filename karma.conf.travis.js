6f3d60eabf07bec24a163f2cd5caaeab6b088b01
Bruno Scopelliti <bru.scopelliti@gmail.com>
2017-04-18 07:20:37 +0200

    Fix karma config for travis, again

--- karma.conf.travis.js
+++ karma.conf.travis.js
@@ -105,7 +105,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots', 'saucelabs'],
 
 
     // web server port
@@ -141,7 +141,8 @@ module.exports = function(config) {
 
 
     sauceLabs: {
-      testName: 'delegate.js'
+      testName: 'delegate.js',
+      startConnect: false
     },
 
 
